9f97fac39e51015d1cd4be29d847a3e67bd8bc2b
Graham Cox <graham@grahamcox.co.uk>
2019-11-01 07:55:44 +0000

    Support for environment config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
+
+import config from 'config';
 import pino from 'pino';
 
 const logger = pino({ name: 'nodeworlds' });
 
-logger.info('Testing: %s', process.env.TEST);
+const httpPort = config.get('http.port');
+
+logger.info('Testing: %s', httpPort);
